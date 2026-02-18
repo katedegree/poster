380d25946aaab2b816ccbee51d4c1bc3d7279c8c
Kate0418 <nakao.keito0418@gmail.com>
2026-02-18 13:40:18 +0900

    ログ出力修正

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # すでにバックグラウンドなら何もしない
 if [ -z "$RUNNING_IN_BG" ]; then
-  RUNNING_IN_BG=1 nohup "$0" "$@" > app.log 2>&1 < /dev/null &
+  RUNNING_IN_BG=1 nohup "$0" "$@" >> app.log 2>&1 &
   disown
   exit 0
 fi
@@ -11,9 +11,12 @@ set -a
 source .env
 set +a
 
-./follow/add.sh "$SESSION_TOKEN" "$USER_ID" &
+# 無限ループしている add 系だけログに出す
+./follow/add.sh "$SESSION_TOKEN" "$USER_ID" >> app.log 2>&1 &
+./like/add.sh "$SESSION_TOKEN" >> app.log 2>&1 &
+
+# all 系はログ不要
 ./follow/all.sh "$SESSION_TOKEN" &
-./like/add.sh "$SESSION_TOKEN" &
 ./like/all.sh "$SESSION_TOKEN" &
 
 wait
